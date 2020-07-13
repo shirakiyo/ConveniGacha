27cc0eba1673f301ebe6a43dc229371b32e4d324
shirakiyo <tus.sk5678@gmail.com>
2020-07-13 19:42:42 +0900

    🛠  fix test

--- internal/usecase/product_test.go
+++ internal/usecase/product_test.go
@@ -37,7 +37,7 @@ func (cs ProductTestCases) Run(t *testing.T) {
 	}
 }
 
-func TestProductUseCase_GetProduct(t *testing.T) {
+func TestProductUseCase_GetFamimaProduct(t *testing.T) {
 	expectedProducts := []*model.Product{
 		{
 			Name:   "foo",
@@ -50,7 +50,7 @@ func TestProductUseCase_GetProduct(t *testing.T) {
 		{
 			name: "category is nil OK",
 			run: func(t *testing.T, u ProductUseCase) error {
-				product, err := u.GetProduct("")
+				product, err := u.GetFamimaProduct("")
 
 				if product == nil {
 					err = errors.New("product is nil")
@@ -71,7 +71,7 @@ func TestProductUseCase_GetProduct(t *testing.T) {
 		{
 			name: "category is foods OK",
 			run: func(t *testing.T, u ProductUseCase) error {
-				product, err := u.GetProduct(FoodsPrefix)
+				product, err := u.GetFamimaProduct(FoodsPrefix)
 
 				if product == nil {
 					err = errors.New("product is nil")
@@ -92,7 +92,7 @@ func TestProductUseCase_GetProduct(t *testing.T) {
 		{
 			name: "category is sweets OK",
 			run: func(t *testing.T, u ProductUseCase) error {
-				product, err := u.GetProduct(SweetsPrefix)
+				product, err := u.GetFamimaProduct(SweetsPrefix)
 
 				if product == nil {
 					err = errors.New("product is nil")
@@ -113,7 +113,7 @@ func TestProductUseCase_GetProduct(t *testing.T) {
 		{
 			name: "category is foods OK",
 			run: func(t *testing.T, u ProductUseCase) error {
-				product, err := u.GetProduct(SnacksPrefix)
+				product, err := u.GetFamimaProduct(SnacksPrefix)
 
 				if product == nil {
 					err = errors.New("product is nil")
@@ -134,7 +134,7 @@ func TestProductUseCase_GetProduct(t *testing.T) {
 		{
 			name: "category is foods OK",
 			run: func(t *testing.T, u ProductUseCase) error {
-				product, err := u.GetProduct(SnacksPrefix)
+				product, err := u.GetFamimaProduct(SnacksPrefix)
 
 				if product == nil {
 					err = errors.New("product is nil")
@@ -155,7 +155,141 @@ func TestProductUseCase_GetProduct(t *testing.T) {
 		{
 			name: "invalid category",
 			run: func(t *testing.T, u ProductUseCase) error {
-				_, err := u.GetProduct("hoge")
+				_, err := u.GetFamimaProduct("hoge")
+				return err
+			},
+			mock: func(mock *mock_repository.MockProductRepository) {
+			},
+			check: func(t *testing.T, err error) {
+				if !errors.Is(err, ErrInvalidCategory) {
+					t.Errorf("wrong error: %v", err)
+				}
+			},
+		},
+	}
+
+	testCases.Run(t)
+}
+
+func TestProductUseCase_GetLawsonProduct(t *testing.T) {
+	expectedProducts := []*model.Product{
+		{
+			Name:   "foo",
+			Price:  100,
+			Link:   "bar",
+			Detail: "baz",
+		},
+	}
+	testCases := ProductTestCases{
+		{
+			name: "category is nil OK",
+			run: func(t *testing.T, u ProductUseCase) error {
+				product, err := u.GetLawsonProduct("")
+
+				if product == nil {
+					err = errors.New("product is nil")
+				}
+				return err
+			},
+			mock: func(mock *mock_repository.MockProductRepository) {
+				mock.EXPECT().
+					ListProducts(LawsonProductsCSV).
+					Return(expectedProducts, nil)
+			},
+			check: func(t *testing.T, err error) {
+				if err != nil {
+					t.Error(err)
+				}
+			},
+		},
+		{
+			name: "category is foods OK",
+			run: func(t *testing.T, u ProductUseCase) error {
+				product, err := u.GetLawsonProduct(FoodsPrefix)
+
+				if product == nil {
+					err = errors.New("product is nil")
+				}
+				return err
+			},
+			mock: func(mock *mock_repository.MockProductRepository) {
+				mock.EXPECT().
+					ListProducts(LawsonFoodsCSV).
+					Return(expectedProducts, nil)
+			},
+			check: func(t *testing.T, err error) {
+				if err != nil {
+					t.Error(err)
+				}
+			},
+		},
+		{
+			name: "category is sweets OK",
+			run: func(t *testing.T, u ProductUseCase) error {
+				product, err := u.GetLawsonProduct(SweetsPrefix)
+
+				if product == nil {
+					err = errors.New("product is nil")
+				}
+				return err
+			},
+			mock: func(mock *mock_repository.MockProductRepository) {
+				mock.EXPECT().
+					ListProducts(LawsonSweetsCSV).
+					Return(expectedProducts, nil)
+			},
+			check: func(t *testing.T, err error) {
+				if err != nil {
+					t.Error(err)
+				}
+			},
+		},
+		{
+			name: "category is foods OK",
+			run: func(t *testing.T, u ProductUseCase) error {
+				product, err := u.GetLawsonProduct(SnacksPrefix)
+
+				if product == nil {
+					err = errors.New("product is nil")
+				}
+				return err
+			},
+			mock: func(mock *mock_repository.MockProductRepository) {
+				mock.EXPECT().
+					ListProducts(LawsonSnacksCSV).
+					Return(expectedProducts, nil)
+			},
+			check: func(t *testing.T, err error) {
+				if err != nil {
+					t.Error(err)
+				}
+			},
+		},
+		{
+			name: "category is foods OK",
+			run: func(t *testing.T, u ProductUseCase) error {
+				product, err := u.GetLawsonProduct(SnacksPrefix)
+
+				if product == nil {
+					err = errors.New("product is nil")
+				}
+				return err
+			},
+			mock: func(mock *mock_repository.MockProductRepository) {
+				mock.EXPECT().
+					ListProducts(LawsonSnacksCSV).
+					Return(expectedProducts, nil)
+			},
+			check: func(t *testing.T, err error) {
+				if err != nil {
+					t.Error(err)
+				}
+			},
+		},
+		{
+			name: "invalid category",
+			run: func(t *testing.T, u ProductUseCase) error {
+				_, err := u.GetLawsonProduct("hoge")
 				return err
 			},
 			mock: func(mock *mock_repository.MockProductRepository) {
